ec9c8285b3ae4192921b84f5a8474a2d73c0bd09
Chijioke Divine <152617348+Tekdiverse@users.noreply.github.com>
2024-11-26 05:22:37 +0100

    ...

--- profitnow/settings.py
+++ profitnow/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@a0hr9$%m*-$#iye#*cani^=i@k7^50sha_4^=gdvyw=6@wl5$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
